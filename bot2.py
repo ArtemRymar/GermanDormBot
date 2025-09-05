fbdbbf5a2d0f922e72041de63219c6f5cedf71d0
ArtemRymar <95047327+ArtemRymar@users.noreply.github.com>
2025-09-05 08:57:43 +0200

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -50,11 +50,11 @@ async def main():
     counter = 0
     while True:
         await check_offers()
-        if counter % 160 == 0:
+        if counter % 3600 == 0:
             await bot.send_message(chat_id=CHAT_ID, text="✅ Бот работает. Проверки выполняются.")
 
         counter += 1
-        await asyncio.sleep(90)   # каждые 3 минут
+        await asyncio.sleep(1)   # каждые 3 минут
 
 if __name__ == "__main__":
     asyncio.run(main())
