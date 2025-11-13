b5777bec3281be97c9646135950b2a0ea03c38b5
ArtemRymar <95047327+ArtemRymar@users.noreply.github.com>
2025-11-13 17:44:24 +0100

    Update bot to send messages to multiple chat IDs

--- bot2.py
+++ bot2.py
@@ -5,7 +5,7 @@ from telegram import Bot
 
 # Твой токен и chat_id (user_id)
 TOKEN = "8104546885:AAGI4mGtG2xKi-GsKoHjxKRI7DZ4QDR60Ks"
-CHAT_ID = 544553533
+CHAT_ID = [544553533, 362700980]
 
 # Ссылка на страницу общежитий
 URL = "https://www.stwdo.de/en/living-houses-application/current-housing-offers#residential-offer-list"
@@ -32,29 +32,33 @@ async def check_offers():
                 print("Нет предложений.")
             else:
                 print("Есть предложения!")
-                await bot.send_message(chat_id=CHAT_ID, text="⚠️ Появились предложения в общежитии! Проверь сайт!")
+                for chat_id in CHAT_ID:
+                    await bot.send_message(chat_id=chat_id, text="⚠️  THERE ARE NEW OFFERS! Check the website!" + URL)
         else:
             print("Есть предложения (блок отсутствует)!")
-            await bot.send_message(chat_id=CHAT_ID, text="⚠️ Появились предложения в общежитии! Проверь сайт!")
+            for chat_id in CHAT_ID:
+                await bot.send_message(chat_id=chat_id, text="⚠️  THERE ARE NEW OFFERS! Check the website!" + URL)
 
         if offers:
             print("Найдено объявлений:")
-            await bot.send_message(chat_id=CHAT_ID, text="Найдено объявлений:")
+            # await bot.send_message(chat_id=CHAT_ID, text="Найдено объявлений:")
             
 
     except Exception as e:
         print(f"Ошибка при проверке сайта: {e}")
-        await bot.send_message(chat_id=CHAT_ID, text=f"Бот столкнулся с ошибкой: {e}")
+        for chat_id in CHAT_ID:
+            await bot.send_message(chat_id=chat_id, text=f"The bot encountered an error.: {e}")
 
 async def main():
     counter = 0
     while True:
         await check_offers()
         if counter % 3600 == 0:
-            await bot.send_message(chat_id=CHAT_ID, text="✅ Бот работает. Проверки выполняются.")
+            for chat_id in CHAT_ID:
+                await bot.send_message(chat_id=chat_id, text="✅ The bot is working. Checks are being performed.")
 
         counter += 1
-        await asyncio.sleep(1)   # каждые 3 минут
+        await asyncio.sleep(1)   # каждые 5 минут
 
 if __name__ == "__main__":
     asyncio.run(main())
